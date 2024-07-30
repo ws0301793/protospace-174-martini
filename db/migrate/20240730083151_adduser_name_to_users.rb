47d0d9a4f10e332e89c3b3dd68b97aca5aaaacc7
y-west24 <y.west.24.19@gmail.com>
2024-07-30 18:09:32 +0900

    カラムの追加

---
+++ db/migrate/20240730083151_adduser_name_to_users.rb
@@ -0,0 +1,4 @@
+class AdduserNameToUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_30_073645) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_30_090736) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_30_073645) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "username"
+    t.string "profile"
+    t.string "occupation"
+    t.string "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
