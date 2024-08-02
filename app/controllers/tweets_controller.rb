5878d9435e5039315b6659ca433979420bbf453c
y-west24 <y.west.24.19@gmail.com>
2024-08-03 04:32:48 +0900

    createアクションをコントローラーに設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,16 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :catchcopy,:concept,:image)
+  end
+  
 end
