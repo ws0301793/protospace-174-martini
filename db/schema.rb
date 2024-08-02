9b4034deef73702ef483a5060cb373792542ce2e
y-west24 <y.west.24.19@gmail.com>
2024-08-02 18:42:16 +0900

    tweetテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_30_090736) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_02_093040) do
+  create_table "tweets", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
