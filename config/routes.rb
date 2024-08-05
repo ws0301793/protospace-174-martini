c8c0c556785fff83112475c128eb4772808dd2f7
y-west24 <y.west.24.19@gmail.com>
2024-08-05 22:43:05 +0900

    ユーザー名を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create,:show]
+  resources :users, only: [:show]
+  resources :prototypes, only: [:index, :new, :create, :show]
  
 end
