184af18e7a8104097c9aaf9d631454de579a37d4
ws0301793 <wtr290503@gmail.com>
2024-08-06 21:27:49 +0900

    editアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit]
  
 end
