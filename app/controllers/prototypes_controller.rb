426c5761ac5f1058e6c48424a628cab1b1e5b752
y-west24 <y.west.24.19@gmail.com>
2024-08-07 20:20:01 +0900

    プロトタイプコントローラーの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -22,6 +24,19 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
   
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to @prototype, notice: 'プロトタイプ情報が更新されました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to prototypes_path, notice: 'プロトタイプを削除しました'
+  end
+
   private
 
   def prototype_params
