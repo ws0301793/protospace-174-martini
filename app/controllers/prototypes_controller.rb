da5930dd959c1cda0b4338aa7c3f530cb13d562c
y-west24 <y.west.24.19@gmail.com>
2024-08-08 01:41:12 +0900

    今までの機能を修正しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:new, :create, :edit, :update, :destroy, :index, :show]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -17,24 +18,21 @@ class PrototypesController < ApplicationController
 
   def create
     @prototype = current_user.prototypes.new(prototype_params)
-    @comment = @prototype.comments.create(comment_params.merge(user: current_user))
     if @prototype.save
       if params[:comment]
         @prototype.comments.create(comment_params.merge(user: current_user))
       end
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :new
     end
-    if @comment.save
-      redirect_to prototype_path(@prototype)
-    else
-      redirect_to prototype_path(@prototype)
-    end
+  end
+  
+  def edit
+    @prototype = Prototype.find(params[:id])
+      redirect_to root_path unless current_user == @prototype.user
   end
 
-  
-  
   def update
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
@@ -43,15 +41,11 @@ class PrototypesController < ApplicationController
     end
   end
 
-  def edit
-    @prototype = Prototype.find(params[:id])
-      redirect_to root_path unless current_user == @prototype.user
-  end
-
+  
   def destroy
     @prototype = Prototype.find(params[:id])
     @prototype.destroy
-    redirect_to root_path
+    redirect_to prototypes_path
   end
 
 
@@ -61,9 +55,17 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def correct_user
+    redirect_to root_path unless current_user == @prototype.user
+  end
+
 
   def prototype_params
     params.require(:prototype).permit(:name, :catch_copy, :concept, :user_id, :image)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 
   def comment_params
