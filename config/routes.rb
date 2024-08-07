d20203654689eeb4c1823debfed88766a4b8d730
y-west24 <y.west.24.19@gmail.com>
2024-08-07 10:50:58 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :users, only: [:show]
-  resources :prototypes, only: [:index, :new, :create, :show,:edit]
+  resources :prototypes, only: [:index, :new, :create, :show,:edit,:update]
  
 end
