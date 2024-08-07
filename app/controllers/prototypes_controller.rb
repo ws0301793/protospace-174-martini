57173235a8845932a04496b9ee60e4fa0451b9d7
y-west24 <y.west.24.19@gmail.com>
2024-08-07 10:48:10 +0900

    コントローラーの再設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :set_prototype, only: [:edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -19,6 +20,9 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @prototype.update(prototype_params)
       redirect_to @prototype
@@ -32,4 +36,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:name, :catch_copy, :concept, :user_id, :image)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path,unless @prototype.user == current_user
 end
