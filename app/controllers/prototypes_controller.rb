a6d42d6fce9422c05b09622c53d60949f39a4f3d
y-west24 <y.west.24.19@gmail.com>
2024-08-07 21:18:23 +0900

    プロトタイプコントローラーの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
