24cb024a308ba765de86086751a1c54c478ae318
y-west24 <y.west.24.19@gmail.com>
2024-08-08 23:24:36 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :prototypes, only: [:index, :new, :create, :show,:edit,:update]
   resources :prototypes do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   
 end
