d754c7fe154093825f7157e0956c41ff19742d34
y-west24 <y.west.24.19@gmail.com>
2024-08-09 00:00:44 +0900

    ルーティングの再設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :users, only: [:show]
-  resources :prototypes, only: [:index, :new, :create, :show,:edit,:update]
-  resources :prototypes do
-    post 'create_comment'
-    resources :comments, only: [:create, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
   end
-  
 end
