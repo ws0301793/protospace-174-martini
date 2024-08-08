604a593f362554560c2c22ce5078557db8f18340
y-west24 <y.west.24.19@gmail.com>
2024-08-08 22:49:45 +0900

    コントローラーの設定

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,6 +1,26 @@
 class Comment < ApplicationRecord
+  before_action :authenticate_user!
+
   belongs_to :user
   belongs_to :prototype
 
   validates :content, presence: true
+
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    @comment = @prototype.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to prototype_path(@prototype), notice: 'コメントが投稿されました。'
+    else
+      redirect_to prototype_path(@prototype), alert: 'コメントの投稿に失敗しました。'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
