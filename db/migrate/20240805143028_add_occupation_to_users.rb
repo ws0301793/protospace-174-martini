1729eeea02d584e4ca2ac2522f52f206dae624e2
y-west24 <y.west.24.19@gmail.com>
2024-08-10 13:51:03 +0900

    マイグレーションファイルの変更

--- db/migrate/20240805143028_add_occupation_to_users.rb
+++ db/migrate/20240805143028_add_occupation_to_users.rb
@@ -1,5 +1,7 @@
 class AddOccupationToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :occupation, :string
+    unless column_exists?(:users, :occupation)
+      add_column :users, :occupation, :string
+    end
   end
 end
