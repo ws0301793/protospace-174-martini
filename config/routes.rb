ef01d258951f7ddac239b34d6963b435a5ae5935
ws0301793 <wtr290503@gmail.com>
2024-08-10 10:48:39 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :users, only: [:show]
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :prototypes do
     resources :comments, only: [:create]
   end
 end
